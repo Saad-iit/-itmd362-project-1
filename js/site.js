aa2e573619186dab91e0cd12c65f813787865ec2
Saad-iit <77770818+Saad-iit@users.noreply.github.com>
2022-10-12 01:27:27 -0500

    added variables

---
+++ js/site.js
@@ -0,0 +1,10 @@
+function chkemtpy(){
+	
+  var fname = document.form["registerForm"]["fname"].value
+  var lname = document.form["registerForm"]["lname"].value
+  var phone = document.form["registerForm"]["phone"].value
+  var email = document.form["registerForm"]["email"].value
+  var pass = document.form["registerForm"]["password"].value
+  
+  
+}
