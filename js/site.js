332fe1400254f472b10261718d5d4b4be668f9ac
Saad-iit <77770818+Saad-iit@users.noreply.github.com>
2022-10-12 01:34:36 -0500

    added if statement to check for blank fields

--- js/site.js
+++ js/site.js
@@ -5,6 +5,13 @@ function chkemtpy(){
   var phone = document.forms["registerForm"]["phone"].value
   var email = document.forms["registerForm"]["email"].value
   var pass = document.forms["registerForm"]["password"].value
- 
+  
+  if ((!fname) || (!lname) || (!phone) || (!email) || (!pass) ) {
+    alert("Please fill all the fields");
+    return false;
+
+  } else{
+    alert("You are all signed up! We look forward to meeting you!")
+  }
   
 }
